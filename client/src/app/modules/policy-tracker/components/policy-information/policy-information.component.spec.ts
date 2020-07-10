3beaab607ffc518b0cc3b65c3fca85e0587eb65c
syamdaswani <syamasundar.daswani@gmail.com>
2020-07-10 20:47:15 +0800

    [Syam][Client] Alter Test
    1. Initialize Policy Info Data for test case

--- client/src/app/modules/policy-tracker/components/policy-information/policy-information.component.spec.ts
+++ client/src/app/modules/policy-tracker/components/policy-information/policy-information.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PolicyInformationComponent } from './policy-information.component';
+import {PolicyTrackerComponent} from '../policy-tracker-component/policy-tracker.component';
+import {PolicyInfoModel} from '../../../../shared/models/policy-info.model';
 
 describe('PolicyInformationComponent', () => {
   let component: PolicyInformationComponent;
@@ -16,10 +18,17 @@ describe('PolicyInformationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PolicyInformationComponent);
     component = fixture.componentInstance;
+    component.policyInfoData = new PolicyInfoModel();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /*it(`should have policy info data`, () => {
+    fixture = TestBed.createComponent(PolicyInformationComponent);
+    component = fixture.componentInstance;
+    expect(component.policyInfoData.length).toBeGreaterThan(0);
+  });*/
 });
